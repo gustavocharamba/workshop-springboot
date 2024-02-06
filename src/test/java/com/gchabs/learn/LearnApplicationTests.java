04cebd1ddc9e22f3666694aa7723062656e2b9f9
Gustavo Charamba <gucharamba@gmail.com>
2024-02-06 20:17:31 -0300

    Project created

---
+++ src/test/java/com/gchabs/learn/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gchabs.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
