198e02a31c443b86076bab6bb9f658e567aced1f
Gustavo Charamba <gucharamba@gmail.com>
2024-02-07 11:00:13 -0300

    service layer, component registration

--- src/main/java/com/gchabs/learn/resources/UserResource.java
+++ src/main/java/com/gchabs/learn/resources/UserResource.java
@@ -1,19 +1,35 @@
 package com.gchabs.learn.resources;
 
+import com.gchabs.learn.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.gchabs.learn.entities.User;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-    @GetMapping
-    public ResponseEntity<User> findAll(){
-        User user = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    private UserService service;
 
-        return ResponseEntity.ok().body(user);
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
