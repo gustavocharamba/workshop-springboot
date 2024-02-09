ed042fe400f7c2610bc31fe37fe4acfd4d146740
Gustavo Charamba <gucharamba@gmail.com>
2024-02-09 09:05:42 -0300

    OrderStatus enum

--- src/main/java/com/gchabs/learn/entities/Order.java
+++ src/main/java/com/gchabs/learn/entities/Order.java
@@ -1,6 +1,7 @@
 package com.gchabs.learn.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.gchabs.learn.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -16,15 +17,18 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
     public Order(){}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -42,6 +46,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() throws IllegalAccessException {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
